749a6c199aadf15f9b8db9c3cfdd55957688d9fa
Widium <eddiebennacer@orange.fr>
2022-10-19 19:11:58 +0200

    refactor condition after actions

--- source/execution/philo_state.c
+++ source/execution/philo_state.c
@@ -6,7 +6,7 @@
 /*   By: ebennace <ebennace@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:36:11 by ebennace          #+#    #+#             */
-/*   Updated: 2022/10/19 18:21:05 by ebennace         ###   ########.fr       */
+/*   Updated: 2022/10/19 18:28:25 by ebennace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,32 @@
 
 void philo_eat(t_philo *philo)
 {
-    philo->times->last_eat_time = get_actual_time();
-    printf("philo [%d] wait\n", philo->num);
-    if (philo_can_use_two_fork(philo))
+    if (there_are_not_dead_philos(philo->env))
     {
-        printf("philo [%d] unlock 2 fork\n", philo->num);
-        if (no_more_time_to_eat(philo))
+        philo->times->last_eat_time = get_actual_time();
+        printf("philo [%d] wait\n", philo->num);
+        if (philo_can_use_two_fork(philo))
         {
-            change_state(philo, DEAD);
-            philo->env->philo_dead += 1;
-            printf("philo [%d] DEAD XX\n", philo->num);
-            philo_puts_down_fork(philo);
-            return ;
-        }
-        else
-        {
-            printf("philo [%d] eat...\n", philo->num);
-            change_state(philo, EAT);
-            ms_sleep(philo->times->eat_time);
-            // save_last_eat_time(philo);
-            philo_puts_down_fork(philo);
+            printf("philo [%d] unlock 2 fork\n", philo->num);
+            if (no_more_time_to_eat(philo))
+            {
+                change_state(philo, DEAD);
+                philo->env->philo_dead += 1;
+                printf("philo [%d] DEAD XX\n", philo->num);
+                philo_puts_down_fork(philo);
+                return ;
+            }
+            else
+            {
+                printf("philo [%d] eat...\n", philo->num);
+                change_state(philo, EAT);
+                ms_sleep(philo->times->eat_time);
+                // save_last_eat_time(philo);
+                philo_puts_down_fork(philo);
+            }
         }
     }
+    
 }
 
 int no_more_time_to_eat(t_philo *philo)
@@ -46,16 +50,22 @@ int no_more_time_to_eat(t_philo *philo)
 }
 void philo_sleep(t_philo *philo)
 {
-    printf("philo [%d] sleep...\n", philo->num);
-    change_state(philo, SLEEP);
-    ms_sleep(philo->times->sleep_time);
+    if (there_are_not_dead_philos(philo->env))
+    {
+        printf("philo [%d] sleep...\n", philo->num);
+        change_state(philo, SLEEP);
+        ms_sleep(philo->times->sleep_time);
+    }
 }
 
 void philo_think(t_philo *philo)
 {
-    printf("philo [%d] think...\n", philo->num);
-    change_state(philo, THINK);
-    ms_sleep(philo->times->die_time);
+    if (there_are_not_dead_philos(philo->env))
+    {
+        printf("philo [%d] think...\n", philo->num);
+        change_state(philo, THINK);
+        ms_sleep(philo->times->die_time);
+    }
 }
 
 int philo_can_use_two_fork(t_philo *philo)
