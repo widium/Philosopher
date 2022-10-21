42d985fc709ae7d54d5399c614c4d4329eaf57cb
Widium <eddiebennacer@orange.fr>
2022-10-21 15:20:27 +0200

    update eat function

--- source/state/eat.c
+++ source/state/eat.c
@@ -6,7 +6,7 @@
 /*   By: ebennace <ebennace@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 11:50:43 by ebennace          #+#    #+#             */
-/*   Updated: 2022/10/21 12:16:55 by ebennace         ###   ########.fr       */
+/*   Updated: 2022/10/21 15:15:05 by ebennace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,15 @@ int no_more_time_to_eat(t_philo *philo)
     return (0);
 }
 
-void save_last_eat_time(t_philo *philo)
+int have_time_to_eat(t_philo *philo)
+{
+    if (get_time_pass(
+        philo->last_eat_time, get_actual_time()) < philo->times->die_time)
+        return (1);
+    return (0);
+}
+
+void reset_eat_timer(t_philo *philo)
 {
     philo->last_eat_time = get_actual_time();
 }
